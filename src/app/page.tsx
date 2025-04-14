e9e95f9614be8557dd9c5b48838c9c358b09b2df
Jia Zheng Choo <choojiazheng@Jias-MacBook-Pro.local>
2025-04-14 12:58:23 +0800

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Navbar from "@/components/ui/navbar";
 import Hero from "@/components/sections/hero";
 import CloudSecure from "@/components/sections/cloud-secure";
-import AIApplications from "@/components/sections/ai-applications";
+// import AIApplications from "@/components/sections/ai-applications";
 import ServicesGrid from "@/components/sections/services-grid";
 import WhyMission from "@/components/sections/why-mission";
 import Testimonials from "@/components/sections/testimonials";
